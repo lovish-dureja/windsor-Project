123bdfada1fc9800763d12011aa24ce86db967a9
lovish-dureja <lovishdureja0786@gmail.com>
2021-07-21 00:55:29 -0400

    implemented the service for connecting the data with backend

--- frontend/project/src/app/Pages/login/login.component.ts
+++ frontend/project/src/app/Pages/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../../services/api.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private apiService: ApiService){
+    
+  }
+  ngOnInit() {
+    this.apiService.getData().subscribe((result) => {
+      console.log('result =>', result);
+    })
   }
 
 }
